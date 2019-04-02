a39aaf45fd7916e4d9d0be022a2d7c753003eb3a
unknown <tigerkheng@mymail.mines.edu>
2019-04-01 22:21:46 -0600

    Added more comments and cleaned up a bit

--- DP.py
+++ DP.py
@@ -33,40 +33,45 @@ def solver_hengrui(x, s):
         if i > j:
             return 0
         # check if the result has been computed
-        if mat[i,j] >= 0:
-            return mat[i,j]
+        if mat[i, j] >= 0:
+            return mat[i, j]
         # no reboot case, compared with reboots at day k
         res = basic_process(i, j)
-        for k in range(i+1,j):
+        for k in range(i+1, j):
             tmp = optimal_process(i, k-1) + optimal_process(k+1, j)
             if tmp > res:
                 res = tmp
-                trace[i,j] = int(k)
+                trace[i, j] = int(k)
         # save the result
-        mat[i,j] = res
-        return mat[i,j]
+        mat[i, j] = res
+        return mat[i, j]
     
     # find reboot days, the recursion is similar to how we compute them
     def find_reboot(i, j):
-        reboot = trace[i,j]
+        reboot = trace[i, j]
         if reboot < 0:
             return []
         return find_reboot(i, reboot-1)+[reboot]+find_reboot(reboot+1, j)
 
     # find the amount of data processed on each day
     def get_data_processed(reboot_days):
+        # the parameter reboot_days is a list containing the days where we want to reboot
+        # data_processed is a list that will hold the amount of data processed on each day
         data_processed = []
+        # x and s start on day 1 (index 0)
         x_index = 0
         s_index = 0
+        # reboot_index is used to keep track of when the next reboot day is
         reboot_index = 0
         while len(data_processed) < len(x):
             # if there are no more reboot days, then we don't have to worry about it
             if reboot_index >= len(reboot_days):
+                # the minimum between x and s is the amount of data that will get processed
                 data_processed.append(min(x[x_index], s[s_index]))
                 x_index = x_index + 1
                 s_index = s_index + 1
             else:
-                # if we reboot, no data is processed and s starts over from the beginning
+                # if we reboot, no data is processed and s starts over from the beginning but x still increments
                 if reboot_days[reboot_index] == x_index:
                     data_processed.append(0)
                     reboot_index = reboot_index + 1
@@ -87,7 +92,7 @@ def solver_hengrui(x, s):
     # Print data processed on each day
     for i in data_processed:
         print(int(i), end=" ")
-
+        
 
 if __name__ == "__main__":
     # basic example
@@ -97,4 +102,3 @@ if __name__ == "__main__":
     x = [20, 80, 20, 60, 20, 60, 80, 10, 40, 10]
     s = [100, 90, 50, 45, 40, 35, 20, 15, 10, 5]
     solver_hengrui(x, s)
-    # current result is 365, with a reboot at day 4
